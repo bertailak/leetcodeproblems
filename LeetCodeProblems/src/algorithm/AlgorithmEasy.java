7f262d0b6174d5755a8dc090c53239ae96a08820
s.bertailak <s.bertailak@JIA-024.tscapital.local>
2021-05-26 15:55:54 +0600

    hammingWeight

--- LeetCodeProblems/src/algorithm/AlgorithmEasy.java
+++ LeetCodeProblems/src/algorithm/AlgorithmEasy.java
@@ -2795,6 +2795,15 @@ public class AlgorithmEasy {
         return res;
     }
 
+    public static int hammingWeight(int n) {
+        int count = 0;
+        for (int i = 0; i < 32; i++) {
+            count += (n >> i & 1);
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
-    }   
+
+    }
 }
