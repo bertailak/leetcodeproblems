711bd1a73240593e8ab4cde88f3fdc87007f453c
s.bertailak <s.bertailak@JIA-024.tscapital.local>
2021-05-31 15:11:33 +0600

    islandPerimeter

--- LeetCodeProblems/src/algorithm/AlgorithmEasy.java
+++ LeetCodeProblems/src/algorithm/AlgorithmEasy.java
@@ -2880,6 +2880,46 @@ public class AlgorithmEasy {
         return sum;
     }
 
+    public static int islandPerimeter(int[][] grid) {
+        int perimeter = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                System.out.print(grid[i][j] + " ");
+                if (grid[i][j] == 0) {
+                    if (i > 0 && grid[i - 1][j] == 1) {
+                        perimeter++;
+                    }
+                    if (i < grid.length - 1 && grid[i + 1][j] == 1) {
+                        perimeter++;
+                    }
+                    if (j > 0 && grid[i][j - 1] == 1) {
+                        perimeter++;
+                    }
+                    if (j < grid[0].length - 1 && grid[i][j + 1] == 1) {
+                        perimeter++;
+                    }
+                } else {
+                    if (i == 0) {
+                        perimeter++;
+                    }
+                    if (i == grid.length - 1) {
+                        perimeter++;
+                    }
+                    if (j == 0) {
+                        perimeter++;
+                    }
+                    if (j == grid[0].length - 1) {
+                        perimeter++;
+                    }
+                }
+            }
+        }
+
+        return perimeter;
+    }
+
     public static void main(String[] args) {
+        System.out.println(islandPerimeter(new int[][]{{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}}));
     }
 }
