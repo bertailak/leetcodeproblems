7ea4db04d9566dfa3b5c67805bab4f874ba4dbd5
bertailak <bertailak@gmail.com>
2021-12-10 14:42:17 +0600

    findCenter

--- LeetCodeProblems/src/algorithm/AlgorithmEasy.java
+++ LeetCodeProblems/src/algorithm/AlgorithmEasy.java
@@ -2971,6 +2971,16 @@ public class AlgorithmEasy {
         return result;
     }
 
+    public static int findCenter(int[][] edges) {
+//        https://leetcode.com/problems/find-center-of-star-graph/
+
+        if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) {
+            return edges[0][0];
+        } else {
+            return edges[0][1];
+        }
+    }
+
     public static void main(String[] args) {
     }
 }
