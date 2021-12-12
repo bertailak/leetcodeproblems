799281aa58613c691d493eb66312030e32570e1f
bertailak <bertailak@gmail.com>
2021-12-12 18:42:30 +0600

    tribonacci

--- LeetCodeProblems/src/algorithm/AlgorithmEasy.java
+++ LeetCodeProblems/src/algorithm/AlgorithmEasy.java
@@ -3152,6 +3152,21 @@ public class AlgorithmEasy {
         return profit;
     }
 
+    public static int tribonacci(int n) {
+//        https://leetcode.com/problems/n-th-tribonacci-number/
+
+        int[] a = new int[Math.max(3, n + 1)];
+        a[0] = 0;
+        a[1] = 1;
+        a[2] = 1;
+
+        for (int i = 3; i < a.length; i++) {
+            a[i] = a[i - 1] + a[i - 2] + a[i - 3];
+        }
+
+        return a[n];
+    }
+
     public static void main(String[] args) {
     }
 }
