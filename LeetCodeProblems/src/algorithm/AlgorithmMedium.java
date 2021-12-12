0c1d0aade833498d1c4bb04d36e99de4030378a7
bertailak <bertailak@gmail.com>
2021-12-13 03:22:06 +0600

    productExceptSelf

--- LeetCodeProblems/src/algorithm/AlgorithmMedium.java
+++ LeetCodeProblems/src/algorithm/AlgorithmMedium.java
@@ -62,7 +62,24 @@ public class AlgorithmMedium {
         return merge;
     }
 
+    public static int[] productExceptSelf(int[] nums) {
+//        https://leetcode.com/problems/product-of-array-except-self/
+
+        int n = nums.length;
+        int[] res = new int[n];
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
