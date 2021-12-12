880f4de8d471fa7d6e3e97ebff469039df798a46
bertailak <bertailak@gmail.com>
2021-12-12 15:51:15 +0600

    twoSum, maxProfit

--- LeetCodeProblems/src/algorithm/AlgorithmEasy.java
+++ LeetCodeProblems/src/algorithm/AlgorithmEasy.java
@@ -3117,6 +3117,41 @@ public class AlgorithmEasy {
         return -1;
     }
 
+    public static int[] twoSum(int[] nums, int target) {
+//        https://leetcode.com/problems/two-sum/
+
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[1] = i;
+                result[0] = map.get(target - nums[i]);
+                break;
+            }
+            map.put(nums[i], i);
+        }
+
+        return result;
+    }
+
+    public static int maxProfit(int[] prices) {
+//        https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+        int min = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (min > prices[i]) {
+                min = prices[i];
+            }
+            if (prices[i] - min > profit) {
+                profit = prices[i] - min;
+            }
+        }
+
+        return profit;
+    }
+
     public static void main(String[] args) {
     }
 }
