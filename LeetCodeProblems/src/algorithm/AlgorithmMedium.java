6b8c8c3a94dfb6f836b81b6dcd297b5f4ab892ce
Sherkhan Bertailak <s.bertailak@jusaninvest.com>
2022-02-22 13:45:38 +0600

    characterReplacement

--- LeetCodeProblems/src/algorithm/AlgorithmMedium.java
+++ LeetCodeProblems/src/algorithm/AlgorithmMedium.java
@@ -513,6 +513,28 @@ public class AlgorithmMedium {
         return length;
     }
 
+    public static int characterReplacement(String s, int k) {
+//        https://leetcode.com/problems/longest-repeating-character-replacement/
+
+        int[] alphabet = new int[26];
+
+        int start = 0;
+        int maxLength = 0;
+        int maxFrequency = 0;
+
+        for (int end = 0; end < s.length(); end++) {
+            maxFrequency = Math.max(maxFrequency, ++alphabet[s.charAt(end) - 'A']);
+
+            if (end - start + 1 - maxFrequency > k) {
+                alphabet[s.charAt(start) - 'A']--;
+                start++;
+            }
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+
+        return maxLength;
+    }
+
     public static void main(String[] args) {
     }
 }
